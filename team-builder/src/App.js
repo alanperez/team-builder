d68ecec7954ccb8cbc3a726775019d12ac3fe855
Alan Perez <alanp9six@gmail.com>
2019-07-15 16:55:51 -0500

    :sparkles: Setup the App file by importing components. Components are rendering on screen

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React,{useState} from 'react';
+// import './App.css';
 
 import Form from './components/Form'
+import TeamList from './components/TeamList'
+
 
 function App() {
+  
+  const[teamMember, setTeamMember] = useState({
+    member: '',
+    email: '',
+    role: '',
+  })
+
+  const [membersToEdit, setMembersToEdit] = useState(null)
+
+  const addMember = () => {
+
+  }
+
+  const editMember = () => {
+    
+  }
+
   return (
     <div className="App">
       <Form />
+      <TeamList />
     </div>
   );
 }
