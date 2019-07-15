c34423b75279ee18ba6e705cd7727f4f786c417a
Alan Perez <alanp9six@gmail.com>
2019-07-15 16:58:34 -0500

    Form can handle user inpuit. Only logging input as of right now

--- team-builder/src/components/form.js
+++ team-builder/src/components/form.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+function Form() {
+
+  const[teamMember, setTeamMember] = useState({
+    member: '',
+    email: '',
+    role: '',
+  })
+
+const handleChange = e => {
+  setTeamMember({
+    ...teamMember,
+    [e.target.name]:e.target.value
+  })
+}
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('name:', teamMember.member)
+    console.log('email:', teamMember.email)
+    console.log('role:', teamMember.role)
+  }
   return (
-      <div>Formss</div>
+    <div className='ui middle aligned center aligned grid'>
+      <div>
+      <h3>Team Member</h3>
+      <form onSubmit={ handleSubmit }>
+        <label>
+          Name: 
+          <input name='member' type='text' placeholder='Joe Bob' onChange={handleChange} value={teamMember.member}/>
+        </label>
+
+        <label>
+          E-mail: 
+          <input name='email' type='email' value={teamMember.email} placeholder='joe@bob.com' onChange={handleChange}/>
+        </label>
+
+        <label>
+          Role: 
+          <input name='role' type='text' value={teamMember.role} placeholder='backend engineer, frontend engineer, designer' onChange={handleChange}/>
+        </label>
+
+        <label>
+          <button>Submit</button>
+          <button>Edit</button>
+        </label>
+      </form>
+      </div>
+   </div>
   )
 }
 
