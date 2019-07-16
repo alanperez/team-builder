e064302fbf89063396d458d4d097f173a92d4710
Alan Perez <alanp9six@gmail.com>
2019-07-16 16:06:15 -0500

    :sparkles: Changed up some stuff.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,30 +3,42 @@ import React,{useState} from 'react';
 
 import Form from './components/Form'
 import TeamList from './components/TeamList'
-
+import {team} from '../src/team'
 
 function App() {
   
+  // team member state
   const[teamMember, setTeamMember] = useState({
-    member: '',
+    name: '',
     email: '',
     role: '',
   })
 
+  // data of team
+  const [currentMember, setCurrentMember] = useState(team)
+
+  // edit
   const [membersToEdit, setMembersToEdit] = useState(null)
 
-  const addMember = () => {
-
+  const addMember = member => {
+    setCurrentMember([...currentMember, member])
   }
 
-  const editMember = () => {
-    
+  const editMember = member => {
+     setCurrentMember([...currentMember,member])
   }
 
   return (
     <div className="App">
-      <Form />
-      <TeamList />
+      <Form 
+        teamMember={teamMember}
+        setTeamMember={setTeamMember}
+        addMember={addMember}
+      />
+
+      {currentMember.map(member => {
+        return <TeamList member={member} currentMember={currentMember} addMember={addMember} editMember={editMember}/>
+      })}
     </div>
   );
 }
